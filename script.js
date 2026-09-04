34618a725f8005ca37b64f415922f2160a8438ca
ejdnn <khususpraktikum5@gmail.com>
2026-09-04 10:37:58 +0700

    praktikum2 XI RPL 1

---
+++ script.js
@@ -0,0 +1,15 @@
+function tampilkanNama(){
+    document.getElementById("namaAnggota").innerHTML=
+    ` 
+    <ol style="list-style-type:decimal; padding-left:5%;">
+       <li>Galih (Galih@gmail.com)</li>
+       <li>Fazan (Fazan@gmail.com)</li>
+       <li>Yafi (Yafi@gmail.com)</li>
+       <li>Kevin (Kevinn@gmail.com)</li>
+       <li>Kevin (naya imut lucu kiyut@gmail.com)</li>
+    </ol>
+
+    <button onclick="location.reload()">Tutup Kembali</button>
+    `;
+    
+}
